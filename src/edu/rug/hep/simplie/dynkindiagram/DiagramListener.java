e2cdb9a2017e7c0b6ee472ca17fb6ac408e60835
teake <teake@de14be10-1b9d-11de-a958-c9a343b5b891>
2007-10-18 12:32:37 +0000

    * Added the GPL v3 license.

--- src/edu/rug/hep/simplie/dynkindiagram/DiagramListener.java
+++ src/edu/rug/hep/simplie/dynkindiagram/DiagramListener.java
@@ -3,6 +3,22 @@
  *
  * Created on 18 mei 2007, 16:11
  *
+ *
+ * This file is part of SimpLie.
+ * 
+ * SimpLie is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SimpLie is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SimpLie.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 package edu.rug.hep.simplie.dynkindiagram;
--- src/edu/rug/hep/simplie/ui/DynkinDiagramPanel.java
+++ src/edu/rug/hep/simplie/ui/DynkinDiagramPanel.java
@@ -2,6 +2,22 @@
  * DynkinDiagramPanel.java
  *
  * Created on 20 maart 2007, 17:40
+ *
+ * This file is part of SimpLie.
+ * 
+ * SimpLie is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SimpLie is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SimpLie.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 package edu.rug.hep.simplie.ui;
